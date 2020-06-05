1a6a67d7c7286af32ddcbd7334ee8bf79905e819
Alex <alexdemchenko@yahoo.com>
2020-06-06 01:55:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# React Native Firebase Chat Core
---
+++ example/src/App.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react'
+import { Counter } from 'react-native-firebase-chat-core'
+
+const App = () => {
+  useEffect(() => {
+    console.log('')
+  })
+
+  return <Counter />
+}
+
+export default App
