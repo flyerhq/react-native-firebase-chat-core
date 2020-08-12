7ace2442e2c212f12b3d527dd0a7385ee6bd858a
Alex Demchenko <alexdemchenko@yahoo.com>
2020-08-12 09:19:40 +0200

    Add images support

--- example/src/screens/ChatScreen/ChatScreen.tsx
+++ example/src/screens/ChatScreen/ChatScreen.tsx
@@ -1,10 +1,12 @@
-import { Chat } from '@flyerhq/react-native-chat-ui'
+import { Chat, SendImageCallback } from '@flyerhq/react-native-chat-ui'
 import {
   useFirebaseUser,
   useMessages,
 } from '@flyerhq/react-native-firebase-chat-core'
+import storage from '@react-native-firebase/storage'
 import { RouteProp } from '@react-navigation/native'
 import React from 'react'
+import ImagePicker from 'react-native-image-picker'
 import { MainStackParamList } from 'src/types'
 
 interface Props {
@@ -15,9 +17,31 @@ const ChatScreen = ({ route }: Props) => {
   const { firebaseUser } = useFirebaseUser()
   const { messages, sendMessage } = useMessages(route.params.roomId)
 
+  const handleAttachmentPress = (send: SendImageCallback) => {
+    ImagePicker.showImagePicker(
+      { maxWidth: 1440, noData: true, quality: 0.7 },
+      async (response) => {
+        if (response) {
+          try {
+            const fileName = response.uri.split('/').pop()
+            const reference = storage().ref(fileName)
+            await reference.putFile(response.uri)
+            const imageUrl = await reference.getDownloadURL()
+            send({
+              height: response.height,
+              imageUrl,
+              width: response.width,
+            })
+          } catch {}
+        }
+      }
+    )
+  }
+
   return (
     <Chat
       messages={messages}
+      onAttachmentPress={handleAttachmentPress}
       onSendPress={sendMessage}
       user={{ id: firebaseUser?.uid ?? '', name: '' }}
     />
