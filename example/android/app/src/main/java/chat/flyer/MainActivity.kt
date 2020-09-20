69bf18aed2ad1e7889af69dfaf3f77926f994e6a
Alex Demchenko <alexdemchenko@yahoo.com>
2020-09-20 11:13:48 +0200

    Code cleanup

---
+++ example/android/app/src/main/java/chat/flyer/MainActivity.kt
@@ -0,0 +1,10 @@
+package chat.flyer
+
+import com.facebook.react.ReactActivity
+
+class MainActivity : ReactActivity() {
+
+    override fun getMainComponentName(): String? {
+        return "example"
+    }
+}
